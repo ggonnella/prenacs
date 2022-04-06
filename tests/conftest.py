406b69a60ceea668b909e653df38ffa4a70bbaf7
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2022-04-06 12:07:12 +0200

    move testplugins and testdata under tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,12 +4,12 @@ import os
 @pytest.fixture
 def testdata():
   return lambda fn: os.path.join(\
-      os.path.join(os.path.dirname(__file__),"../testdata/"), fn)
+      os.path.join(os.path.dirname(__file__),"testdata/"), fn)
 
 @pytest.fixture
 def testplugins():
   return lambda fn: os.path.join(\
-      os.path.join(os.path.dirname(__file__),"../testplugins/"), fn)
+      os.path.join(os.path.dirname(__file__),"testplugins/"), fn)
 
 @pytest.fixture
 def examples():
