49aa53e857cb6d567ec4413defa3341781cec99d
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2022-04-13 13:51:45 +0200

    remove code from scripts_helper which was moved to report.py

--- provbatch/scripts_helper.py
+++ provbatch/scripts_helper.py
@@ -1,21 +1,16 @@
 from schema import Or, And, Use
 import sys
 import yaml
-import socket
-import getpass
+from provbatch.report import Report
 
 yamlfile = Or(And(None, Use(lambda n: {})),
               And(Use(open), Use(lambda fn: yaml.safe_load(fn))))
 outfile_or_stderr = Or(And(None, Use(lambda f: sys.stderr)),
                     Use(lambda f: open(f, "w")))
-user = Or(str, And(None, Use(lambda n: getpass.getuser())))
-system = Or(str, And(None, Use(lambda n: socket.gethostname())))
 
-reasons = ["new_data", "new_attributes", "improve_precision"]
-
-report_args_schema = {"--report": outfile_or_stderr, "--user": user,
-                      "--reason": Or(None, lambda r: r in reasons),
-                      "--system": system, "--params": yamlfile}
+report_args_schema = {"--report": outfile_or_stderr, "--user": Or(str, None),
+                      "--reason": Or(None, lambda r: r in Report.REASONS),
+                      "--system": Or(str, None), "--params": yamlfile}
 
 report_args_doc = """\
   --report, -r FN   computation report file (default: stderr)
