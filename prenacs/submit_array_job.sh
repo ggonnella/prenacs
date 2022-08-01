effea4d9eb289259a7d452364a10a235300e7f1c
Ozan Kanbertay <81885929+ozan-kanbertay@users.noreply.github.com>
2022-08-02 00:27:36 +0200

    passed initial live test on Slurm

--- prenacs/submit_array_job.sh
+++ prenacs/submit_array_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=prenacs_job_array
-#SBATCH --cpus-per-task=24
+#SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=1G
 #SBATCH --constraint=scratch
 #SBATCH --qos=short
@@ -12,7 +12,13 @@ PARAMETERS_FILE=$2
 INPUT_LIST_FILE=$3
 OUTPUT_DIR=$4
 
-prenacs/prenacs-array-task \
-  $PLUGIN_FILE $PARAMETERS_FILE \
-  $INPUT_LIST_FILE $SLURM_ARRAY_TASK_ID \
-  $OUTPUT_DIR
+
+module purge
+eval "$(/usr/users/kanbertay/software/miniconda3/bin/conda shell.bash hook)"
+conda activate prenacs_slurm_env
+
+# TODO: Get the "prenacs-array-task" path without user input
+~/software/prenacs/prenacs/prenacs-array-task \
+ $PLUGIN_FILE $PARAMETERS_FILE \
+ $INPUT_LIST_FILE $SLURM_ARRAY_TASK_ID \
+ $OUTPUT_DIR
