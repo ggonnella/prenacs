ba71d0dcd7f7744977e21624936f40bd0df3cc26
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2022-10-28 16:24:31 +0200

    add setup_verbosity code

--- prenacs/commands/helpers/__init__.py
+++ prenacs/commands/helpers/__init__.py
@@ -2,7 +2,25 @@
 # (c) 2022, Giorgio Gonnella, University of Goettingen, Germany
 #
 
+import prenacs
 from .common import validate
 from . import common
 from . import report
 from . import database
+
+import sys
+from pathlib import Path
+import importlib.util
+
+def setup_verbosity(args):
+  if args["--quiet"]:
+    prenacs.PROGRESS_ENABLED = False
+    prenacs.logger.remove()
+  else:
+    prenacs.PROGRESS_ENABLED = True
+    if args["--debug"]:
+      level = "DEBUG"
+    else:
+      level = "INFO"
+    prenacs.enable_logger(level)
+
